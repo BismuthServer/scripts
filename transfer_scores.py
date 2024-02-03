542ec77ee5c8950bafed36332ce4c0c81b80195d
NoaQT <noa@owo.si>
2024-02-03 18:11:40 +0100

    Fix transfer_scores creating zeros

--- transfer_scores.py
+++ transfer_scores.py
@@ -1,6 +1,7 @@
 import mcrcon
 from nbt import nbt
 import json
+import os
 
 with open("config.json") as f:
     cfg = json.load(f)
@@ -11,12 +12,14 @@ rcon_port = cfg["server"]["rconport"]
 
 smprcon = mcrcon.MCRcon(ip, rcon_pass, rcon_port)
 smprcon.connect()
-scoreboards = nbt.NBTFile("/home/mc/bismuth/survival/server/bismuth_smp/data/scoreboard.dat")["data"]
+
+world_path = cfg['server']['world']
+scoreboards = nbt.NBTFile(os.path.join(world_path, 'data', 'scoreboard.dat'))['data']
 
 new_player = input("Enter new IGN: ")
 old_player = input("Enter old IGN: ")
 
-for i in scoreboards["Objectives"]:
-    smprcon.command("/scoreboard players operation " + new_player + " " + i["Name"].value + " += " + old_player + " " + i["Name"].value)
+for score in filter(lambda x: x['Name'].value == old_player, scoreboards['PlayerScores']):
+    smprcon.command(f"/scoreboard players set {new_player} {score['Objective'].value} {score['Score'].value}")
 
 print("Check if the scores have transferred. If they have, then run '/scoreboard players reset " + old_player + "'")
